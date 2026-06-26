d5768dfe751e6e4b248fdb3d2c27b3005d9b4e91
Darshan Shinde | Cybersecurity Enthusiast <117001697+Darshan1009@users.noreply.github.com>
2026-06-26 20:37:05 +0530

    Change X-Frame-Options value to SAMEORIGIN

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
           },
           {
             key: "X-Frame-Options",
-            value: "DENY",
+            value: "SAMEORIGIN",
           },
           {
             key: "Referrer-Policy",
